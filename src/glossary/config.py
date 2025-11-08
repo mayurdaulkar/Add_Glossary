6f46c37612c534ab2b82b780b650e9936aaab41b
Mayur Daulkar <159690609+mayurdaulkar@users.noreply.github.com>
2025-11-08 16:21:21 +0530

    Update config.py
    E to e in config

--- src/glossary/config.py
+++ src/glossary/config.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Load environment variables from .env filE
+# Load environment variables from .env file
 load_dotenv()
 
 # ==============================================================================
